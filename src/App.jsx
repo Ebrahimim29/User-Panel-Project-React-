19eca5b789466d8e1b94e81fd02ab01d5f67b587
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-12-10 07:25:36 +0330

    User Panel Project-part1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <div className="fixed top-0 left-0 h-screen w-full bg-green-300 pr-64 pt-16">Content</div>
+      <div className="bg-amber-300 fixed top-0 left-0 h-16 w-full pr-64">Header</div>
+      <div className="fixed right-0 h-screen w-64 bg-blue-400">Sidebar</div>
+  </div>
+  )
+};
+
+export default App;
